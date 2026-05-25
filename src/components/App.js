8aaa605b6ae962267c904642a5778e1890d3ccce
Suvam Poddar <98863065+Kousei74@users.noreply.github.com>
2026-05-26 02:34:18 +0530

    Refactor App component to include LoginForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+function LoginForm({ isLoggedIn, setIsLoggedIn }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
 
-const App = () => {
+  if (isLoggedIn) {
+    return <h2>Logged In</h2>;
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Login</button>
+    </form>
+  );
 }
 
-export default App
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div>
+      {/* Do not remove the main div */}
+      <LoginForm
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+      />
+    </div>
+  );
+};
+
+export default App;
